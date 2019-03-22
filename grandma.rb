c4a243a00a2c970cc63b20522d4c0994925d91f4
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-22 19:57:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma (input)
   shout = input.upcase
   if input != shout
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif input = "I LOVE YOU GRANDMA!"
+  elsif input == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else input == shout
     puts "NO, NOT SINCE 1938!"
